34dc7b54d483ba21c56c3ba5ee213d35fa2bed01
PeterSadek <Peter_Sadek@outlook.com>
2024-09-19 01:35:10 +0300

    Install async storage and toast notifications

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native"; // Missing import for NavigationContainer
 import { MyStack } from "./pages/StackNavigator"; // Ensure MyStack is defined and correctly imported
+import { ToastProvider } from "react-native-toast-notifications";
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <MyStack />
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <ToastProvider>
+        <MyStack />
+      </ToastProvider>
+    </NavigationContainer>
+  );
 };
 
 export default App;
