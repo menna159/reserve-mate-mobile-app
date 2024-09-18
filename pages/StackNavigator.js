927d778fdd388c298581fad66d9cbed5ce4ac542
Menna Mahmoud <menna@example.com>
2024-09-18 22:32:49 +0200

    navigation

---
+++ pages/StackNavigator.js
@@ -0,0 +1,14 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './Home';
+import Login from './Login';
+import { routes } from '../routes';
+const Stack = createStackNavigator();
+
+export function MyStack() {
+  return (
+    <Stack.Navigator initialRouteName={routes.login}>
+      <Stack.Screen name={routes.login} component={Login} />
+      <Stack.Screen name={routes.home} component={Home} />
+    </Stack.Navigator>
+  );
+}
